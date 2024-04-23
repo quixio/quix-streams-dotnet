54b723334080d946a3c37fc1d629fd72094754e6
Peter Nagy <peter@quix.io>
2024-04-23 20:48:31 +0100

    Update version to 0.7.1

--- builds/csharp/nuget/build_nugets.py
+++ builds/csharp/nuget/build_nugets.py
@@ -7,8 +7,8 @@ import fileinput
 from typing import List
 
 version = "0.7.1.0"
-informal_version = "0.7.1.0-dev2"
-nuget_version = "0.7.1.0-dev2"
+informal_version = "0.7.1.0"
+nuget_version = "0.7.1.0"
 
 
 def updatecsproj(projfilepath):
