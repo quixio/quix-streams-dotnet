d1b37fdfd7a0572046813486c5360dd665b7f4d9
Peter Nagy <peter@quix.io>
2024-09-04 23:13:02 +0100

    Allow setters for event and param definitions

--- src/QuixStreams.Streaming/Models/ParameterDefinition.cs
+++ src/QuixStreams.Streaming/Models/ParameterDefinition.cs
@@ -9,48 +9,48 @@
         /// <summary>
         /// Gets the unique parameter id
         /// </summary>
-        public string Id { get; internal set; }
+        public string Id { get; set; }
 
         /// <summary>
         /// Gets the human friendly display name of the parameter
         /// </summary>
-        public string Name { get; internal set; }
+        public string Name { get; set; }
 
         /// <summary>
         /// Gets the description of the parameter
         /// </summary>
-        public string Description { get; internal set; }
+        public string Description { get; set; }
 
         /// <summary>
         /// Gets the location of the parameter within the Parameter hierarchy. Example: "/", "car/chassis/suspension".
         /// </summary>
-        public string Location { get; internal set; }
+        public string Location { get; set; }
 
         /// <summary>
         /// Gets the minimum value of the parameter
         /// </summary>
-        public double? MinimumValue { get; internal set; }
+        public double? MinimumValue { get; set; }
         
         /// <summary>
         /// Gets the maximum value of the parameter
         /// </summary>
-        public double? MaximumValue { get; internal set; }
+        public double? MaximumValue { get; set; }
 
         /// <summary>
         /// Gets the unit of the parameter 
         /// </summary>
-        public string Unit { get;internal set; }
+        public string Unit { get;set; }
 
         /// <summary>
         /// Gets the formatting to apply on the value for display purposes
         /// </summary>
-        public string Format { get; internal set; }
+        public string Format { get; set; }
 
         /// <summary>
         /// Gets the optional field for any custom properties that do not exist on the parameter.
         /// For example this could be a json string, describing the optimal value range of this parameter
         /// </summary>
-        public string CustomProperties { get; internal set; }
+        public string CustomProperties { get; set; }
 
         /// <summary>
         /// Converts the Parameter definition to Telemetry layer structure
